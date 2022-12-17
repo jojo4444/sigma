52b3fa1f920abba6a4f72bb781361f91c1d8ebc2
giordano.ch <jojowork4444@gmail.com>
2022-12-17 15:32:42 +0200

    add Dictionary class

---
+++ dict.cpp
@@ -0,0 +1,118 @@
+#include "dict.hpp"
+
+wordStat::wordStat() {
+	add = 0;
+	del = 1;
+}
+wordStat::wordStat(int Add, int Del) {
+	add = Add;
+	del = Del;
+}
+
+
+bool wordStat::operator> (const wordStat& T) const {
+	return std::tie(del, add) > std::tie(T.del, T.add);
+}
+
+Dict::Dict() {
+	vector<int> cnt(26);
+
+	std::ifstream file("../words.txt", std::ifstream::in);
+
+	string word;
+	vector<string> words;
+
+
+	while (file >> word) {
+		if (word.size() < MIN_LEN_WORD) {
+			continue;
+		}
+		words.push_back(word);
+	}
+	file.close();
+
+	// убираем повторы
+	makeunique(words);
+
+	bool empty = true;
+	for (string w : words) {
+		for (char c : w) {
+			if (c < 'a' || c > 'z') {
+				std::cerr << "Bad word: " << w << "\n";
+				exit(-1);
+			}
+			cnt[c - 'a']++;
+		}
+		empty = false;
+		t_.add(w); // инвариант соблюден из-за отсутствия повторов
+	}
+
+	if (empty) {
+		std::cerr << "empty dictionary\n";
+		exit(-1);
+	}
+
+	std::random_device rd;
+	gen_.seed(rd());
+	dist_ = std::discrete_distribution<int>(cnt.begin(), cnt.end());
+}
+
+char Dict::getLet() const {
+	return dist_(gen_) + 'a';
+}
+
+bool Dict::find(const string& word) const {
+	return t_.find(word);
+}
+
+wordStat Dict::getWordStat(const string& word) const {
+	// todo: нужна формула
+	return wordStat();
+}
+
+vector<string> Dict::getHints(const vector<char>& let) const {
+	buffWord_.resize(0);
+	buffHints_.resize(0);
+	buffLet_ = let;
+	generateSubwords();
+
+	makeunique(buffHints_);
+
+	vector<string> inDict;
+	for (const string& w : buffHints_) if (find(w)) {
+		inDict.push_back(w);
+	}
+	std::sort(inDict.begin(), inDict.end(), [&](const string& a, const string& b) {
+		return getWordStat(a) > getWordStat(b);
+	});
+
+	if (inDict.size() > LIMIT_HINTS) {
+		inDict.resize(LIMIT_HINTS);
+	}
+
+	// чтобы итоговая память была строго O(min(LIM, |inDict|)) снаружи
+	vector<string> res;
+	res.reserve(inDict.size()); 
+	for (const string& w : inDict) {
+		res.push_back(w);
+	}
+
+	return res;
+}
+
+void Dict::generateSubwords() const {
+	if (buffWord_.size() > 0) {
+		buffHints_.push_back(buffWord_);
+	}
+	if (buffLet_.size() == buffWord_.size()) {
+		return;
+	}
+	for (char& c : buffLet_) if (c) {
+		char save_c = c;
+		c = 0;
+		buffWord_.push_back(save_c);
+		generateSubwords();
+		buffWord_.pop_back();
+		c = save_c;
+	}
+}
---
+++ dict.hpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+
+#include "trie.hpp"
+
+using std::vector;
+
+const int LIMIT_HINTS = 44;
+const int MIN_LEN_WORD = 5;
+
+struct wordStat {
+	int add, del;
+
+	wordStat();
+	wordStat(int Add, int Del);
+
+	bool operator> (const wordStat& T) const;
+};
+
+class Dict {
+public:
+	Dict();
+
+	char getLet() const;
+	bool find(const string& word) const;
+	wordStat getWordStat(const string& word) const;
+	vector<string> getHints(const vector<char>& let) const;
+
+private:
+	mutable string buffWord_;
+	mutable vector<char> buffLet_;
+	mutable vector<string> buffHints_;
+	void generateSubwords() const;
+
+    mutable std::mt19937 gen_;
+    mutable std::discrete_distribution<int> dist_;    
+    Trie t_;
+};
+
+template <typename T>
+void makeunique(vector<T>& v) {
+	std::sort(v.begin(), v.end());
+	v.erase(unique(v.begin(), v.end()), v.end());
+}
