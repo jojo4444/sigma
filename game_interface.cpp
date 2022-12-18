f60c60b670583262a75795061b96688ff3f759db
Gleb Alekseev <azzar.bam@gmail.com>
2022-12-18 15:05:43 +0200

    amend merge conflicts

--- game_interface.cpp
+++ game_interface.cpp
@@ -6,43 +6,43 @@ void GameInterface::drawLettersBar(const vector<char>& letters, int player){
     shiftRow(rowNum - 1);
     for (int i = 3, j = 0; j < letters.size(); j++, i += 4){
         shiftCol(i - 1);
-        cells[rowNum - 1][i - 1].sym = '╔';
-        cells[rowNum - 1][i - 1].paint();
+        tabs[rowNum - 1][i - 1].sym = '╔';
+        tabs[rowNum - 1][i - 1].paint();
         shiftCol(i);
-        cells[rowNum - 1][i].sym = '═';
-        cells[rowNum - 1][i].paint()
+        tabs[rowNum - 1][i].sym = '═';
+        tabs[rowNum - 1][i].paint()
         shiftCol(i + 1);
-        cells[rowNum - 1][i + 1].sym = '╗';
-        cells[rowNum - 1][i + 1].paint()
+        tabs[rowNum - 1][i + 1].sym = '╗';
+        tabs[rowNum - 1][i + 1].paint()
     }
     shiftToBeginCol();
     shiftRow(rowNum + 1);
     for (int i = 3, j = 0; j < letters.size(); j++, i += 4){
         shiftCol(i - 1);
-        cells[rowNum + 1][i - 1].sym = '╚';
-        cells[rowNum + 1][i - 1].paint()
+        tabs[rowNum + 1][i - 1].sym = '╚';
+        tabs[rowNum + 1][i - 1].paint()
         shiftCol(i);
-        cells[rowNum + 1][i].sym = '═';
-        cells[rowNum + 1][i].paint()
+        tabs[rowNum + 1][i].sym = '═';
+        tabs[rowNum + 1][i].paint()
         shiftCol(i + 1);
-        cells[rowNum + 1][i + 1].sym = '╝';
-        cells[rowNum + 1][i + 1].paint()
+        tabs[rowNum + 1][i + 1].sym = '╝';
+        tabs[rowNum + 1][i + 1].paint()
     }
     shiftToBeginCol();
     shiftRow(rowNum);
     for (int i = 3, j = 0; j < letters.size(); j++, i += 4){
         
         shiftCol(i - 1);
-        cells[rowNum][i - 1].sym = '║';
-        cells[rowNum][i - 1].paint();
+        tabs[rowNum][i - 1].sym = '║';
+        tabs[rowNum][i - 1].paint();
 
         shiftCol(i);
-        cells[rowNum][i].sym = letters[j];
-        cells[rowNum][i].paint();
+        tabs[rowNum][i].sym = letters[j];
+        tabs[rowNum][i].paint();
 
         shiftCol(i + 1);
-        cells[rowNum][i - 1].sym = '║';
-        cells[rowNum][i - 1].paint();
+        tabs[rowNum][i - 1].sym = '║';
+        tabs[rowNum][i - 1].paint();
     }
     shiftToBeginCol();
     shiftRow(-rowNum);
@@ -50,22 +50,22 @@ void GameInterface::drawLettersBar(const vector<char>& letters, int player){
 }
 
 void GameInterface::drawHpBar(int hp, int player){
-  auto str = std::to_string(hp);
-  int rowNum = player == 1? 18 : 10
-  shiftCol(-85);
-  shiftRow(rowNum);
-  const int mcol = 6;
-  for (int i = (str.size() >> 1), j = mcol; i >= 0; --i, --j){
-    cells[rowNum][j].sym = str[i];
-    cells[rowNum][j].paint();
-  }
-  for (int i = (str.size() >> 1) + 1, j = mcol + 1; i < str.size(); ++i, ++j){
-    cells[rowNum][j].sym = str[i];
-    cells[rowNum][j].paint();
-  }
-  shiftToBeginCol();
-  shiftRow(-rowNum);
-  shiftCol(85);
+    auto str = std::to_string(hp);
+    int rowNum = player == 1? 18 : 10
+    shiftCol(-85);
+    shiftRow(rowNum);
+    const int mcol = 6;
+    for (int i = (str.size() >> 1), j = mcol; i >= 0; --i, --j){
+        tabs[rowNum][j].sym = str[i];
+        tabs[rowNum][j].paint();
+    }
+    for (int i = (str.size() >> 1) + 1, j = mcol + 1; i < str.size(); ++i, ++j){
+        tabs[rowNum][j].sym = str[i];
+        tabs[rowNum][j].paint();
+    }
+    shiftToBeginCol();
+    shiftRow(-rowNum);
+    shiftCol(85);
 }
 #include "game_interface.hpp"
 
